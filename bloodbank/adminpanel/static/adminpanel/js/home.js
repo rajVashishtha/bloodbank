e9a73fcc3980161b426ec2407086aacb8ed3bff7
Raj Vashishtha <vashiraj2000@gmail.com>
2020-02-23 11:40:28 +0530

    Register form completed

--- bloodbank/adminpanel/static/adminpanel/js/home.js
+++ bloodbank/adminpanel/static/adminpanel/js/home.js
@@ -1,15 +1,19 @@
 function change(){
     if( $(".login").css("display") == "block"){
         $(".navbutton button").text("Login")
-        $(".design").text("Register")
+        $(".contact2-form-title").text("Register")
     $(".login").css({'display':'none'})
     $(".signup").css({'display':'block'})
     }
     else{
-        $(".design").text("Login")
+        $(".contact2-form-title").text("Login")
         $(".navbutton button").text("Register")
         $(".signup").css({'display':'none'})
         $(".login").css({'display':'block'})
         
     }
+}
+function reset(){
+    $("input").val("")
+    $("input").text("")
 }
