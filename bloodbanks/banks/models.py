bdadab4416574d19e6fc8fe326ba748b9b6d08d5
Raj Vashishtha <vashiraj2000@gmail.com>
2020-02-23 11:41:38 +0530

    Register form completed

---
+++ bloodbanks/banks/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+class State(models.Model):
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
+class BloodBank(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=64)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name="banks")
+    pincode = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.name} in {self.city}, {self.state}"
+
+
+class BloodGroup(models.Model):
+    group = models.CharField(max_length=2)
+    positive = models.BooleanField()
+
+    def __str__(self):
+        if self.positive:
+            return f"{self.group}+"   
+        return f"{self.group}-"
+
+
+
+class Size(models.Model):
+    size_in_ml = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.size_in_ml} ml"
+
+
+class BloodBag(models.Model):
+    blood_bank = models.ForeignKey(BloodBank,on_delete=models.CASCADE, related_name="bloodbags")
+    blood_group = models.ForeignKey(BloodGroup, on_delete=models.CASCADE, related_name="available")
+    size_in_ml = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="available")
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.blood_bank} has {self.quantity} bags ({self.size_in_ml}) of {self.blood_group} group"
+
+
+
+
